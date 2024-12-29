0ab4484084dd24f247d290e8bec5a021d4b79576
R5-H1 <pclnxt@gmail.com>
2024-12-29 16:49:40 +0900

    추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-    "Test text"
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). 추가 텍스트"
 )
